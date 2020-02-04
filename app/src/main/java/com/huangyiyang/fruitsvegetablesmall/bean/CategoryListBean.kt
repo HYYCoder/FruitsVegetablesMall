4a9a2a1aa000c25f3bb8b33390250fce64f82793
HYYCoder <1176075245@qq.com>
2020-02-04 22:29:34 +0800

    更新

--- app/src/main/java/com/huangyiyang/fruitsvegetablesmall/bean/CategoryListBean.kt
+++ app/src/main/java/com/huangyiyang/fruitsvegetablesmall/bean/CategoryListBean.kt
@@ -2,25 +2,77 @@ package com.huangyiyang.fruitsvegetablesmall.bean
 
 import android.os.Parcel
 import android.os.Parcelable
+import java.util.*
 
-class CategoryListBean() : Parcelable {
+data class CategoryListBean(var category: CategoryBean?,var subCategories: List<SubCategoriesBean>?) : Parcelable {
 
-    var category: CategoryBean? = null
-    var subCategories: List<SubCategoriesBean>? = null
+    constructor(parcel: Parcel) : this(
+        parcel.readParcelable(CategoryBean::class.java.classLoader),
+        parcel.createTypedArrayList(SubCategoriesBean)
+    ) {
+    }
 
-    class CategoryBean{
+    class CategoryBean(var id:Int?,var name:String?,var imageUrl:String?) : Parcelable{
+        constructor(parcel: Parcel) : this(
+            parcel.readValue(Int::class.java.classLoader) as? Int,
+            parcel.readString(),
+            parcel.readString()
+        ) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeValue(id)
+            parcel.writeString(name)
+            parcel.writeString(imageUrl)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<CategoryBean> {
+            override fun createFromParcel(parcel: Parcel): CategoryBean {
+                return CategoryBean(parcel)
+            }
+
+            override fun newArray(size: Int): Array<CategoryBean?> {
+                return arrayOfNulls(size)
+            }
+        }
 
     }
 
-    class SubCategoriesBean{
-        var id = 0
-    }
+    class SubCategoriesBean(var id:Int?,var name:String?) : Parcelable{
+        constructor(parcel: Parcel) : this(
+            parcel.readValue(Int::class.java.classLoader) as? Int,
+            parcel.readString()
+        ) {
+        }
 
-    constructor(parcel: Parcel) : this() {
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeValue(id)
+            parcel.writeString(name)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<SubCategoriesBean> {
+            override fun createFromParcel(parcel: Parcel): SubCategoriesBean {
+                return SubCategoriesBean(parcel)
+            }
+
+            override fun newArray(size: Int): Array<SubCategoriesBean?> {
+                return arrayOfNulls(size)
+            }
+        }
+
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
-
+        parcel.writeParcelable(category, flags)
+        parcel.writeTypedList(subCategories)
     }
 
     override fun describeContents(): Int {
